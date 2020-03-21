26d07d0ea3b6ce27403275e15d4043b934f65484
Shaun <loglution@gmail.com>
2020-03-21 17:00:23 -0500

    Support for dashboard
    Support for dashboard

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import { Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/component.homepage';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Dashboard from './dashboard/Dashboard';
 
 function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage} />
       <Route exact path='/shop' component={ShopPage} />
+      <Route exact path='/dashboard' component={Dashboard} />
+
     </div>  
   );                       
 }
