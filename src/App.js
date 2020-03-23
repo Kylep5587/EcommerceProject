10435dfc9216fe2512a37c5a02abb29271702105
Kyle Price <kylep5587@gmail.com>
2020-03-23 10:36:59 -0400

    Header component addition

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/component.homepage';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 import Dashboard from './dashboard/Dashboard';
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
-      <Route exact path='/dashboard' component={Dashboard} />
-
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/dashboard' component={Dashboard} />
+      </Switch>
     </div>  
   );                       
 }
